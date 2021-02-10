4c4f0240a48ca54f9dec60292bd52bade7ce449e
Mikhail Murtazaliev <volboy@yandex.ru>
2021-02-10 12:44:00 +0300

    Но на самом деле runOnUiThread работает через handler, поэтому используем его

--- app/src/main/java/com/example/appthread/MainActivity.java
+++ app/src/main/java/com/example/appthread/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btn1).setOnClickListener(v -> {
-            myThread = new MyThread(this);
+            myThread = new MyThread(this, new Handler());
             myThread.start();
         });
 
@@ -29,37 +30,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private static void longTask(Activity activity) {
-        try {
-            Thread.sleep(3000);
-            //Log.i("MY_THREAD", "Task was done");
-            activity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(activity, "Task was done", Toast.LENGTH_LONG).show();
-                }
-            });
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
     private static class MyThread extends Thread {
         private Activity activity;
+        private Handler handler;
 
-        public MyThread(Activity activity) {
+        public MyThread(Activity activity, Handler handler) {
             super();
             this.activity = activity;
+            this.handler = handler;
         }
 
         @Override
         public void run() {
-            for (int i = 0; i < 200; i++) {
-                if (isInterrupted()) {
-                    return;
-                }
-                longTask(activity);
+            try {
+                Thread.sleep(3000);
+                //Log.i("MY_THREAD", "Task was done");
+                //activity.runOnUiThread(() -> Toast.makeText(activity, "Task was done", Toast.LENGTH_LONG).show());
+                handler.post(() -> Toast.makeText(activity, "Task was done", Toast.LENGTH_LONG).show());
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
