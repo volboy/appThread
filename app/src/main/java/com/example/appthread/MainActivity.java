b109dd016d5fed4a851107c27894d4c862ede546
Mikhail Murtazaliev <volboy@yandex.ru>
2021-02-10 13:09:36 +0300

    Общаемся с главным потоком через handler.sendEmptyMessage

--- app/src/main/java/com/example/appthread/MainActivity.java
+++ app/src/main/java/com/example/appthread/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.appthread;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -21,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btn1).setOnClickListener(v -> {
-            myThread = new MyThread(this, new Handler());
+            myThread = new MyThread(this, handler);
             myThread.start();
         });
 
@@ -30,6 +34,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        private static final int STATE_START = 0;
+        private static final int STATE_LOADING = 1;
+        private static final int STATE_DONE = 2;
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case STATE_START:
+                    Toast.makeText(MainActivity.this, "START", Toast.LENGTH_SHORT).show();
+                    break;
+                case STATE_LOADING:
+                    Toast.makeText(MainActivity.this, "LOADING", Toast.LENGTH_SHORT).show();
+                    break;
+                case STATE_DONE:
+                    Toast.makeText(MainActivity.this, "STOP", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    };
 
 
     private static class MyThread extends Thread {
@@ -45,11 +74,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             try {
+                handler.sendEmptyMessage(0);
                 Thread.sleep(3000);
                 //Log.i("MY_THREAD", "Task was done");
                 //activity.runOnUiThread(() -> Toast.makeText(activity, "Task was done", Toast.LENGTH_LONG).show());
                 //handler.post(() -> Toast.makeText(activity, "Task was done", Toast.LENGTH_LONG).show());
                 new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(activity, "Task was done", Toast.LENGTH_LONG).show());
+                handler.sendEmptyMessage(2);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
